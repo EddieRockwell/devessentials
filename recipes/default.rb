028b306ab97bd00ae7cd40a259248c8c10e915ad
Ed Rockwell <eddierockwell@gmail.com>
2018-05-21 12:31:27 -0500

    adds apache recipe to recipes/default.rb

---
+++ recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: devessentials
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+# This is my first edit
+# And again another edit
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
